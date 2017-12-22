4f6902a9d5b5e2f51d39b28cc65c1974d488f5fd
Tristan VALCKE <valcketristan@gmail.com>
2017-12-23 00:25:03 +0100

    Config: revert ignore of the intermediary file that are required in sources

--- configs/es6.conf.js
+++ configs/es6.conf.js
@@ -17,9 +17,9 @@ module.exports = {
 		'Three.js',
 		'polyfills.js',
 		'libs',
-		'Curves.js',						// Ignore intermediary exporter files
-		'Geometries.js',					// Ignore intermediary exporter files
-		'Materials.js',						// Ignore intermediary exporter files
+		//		'Curves.js',						// Ignore intermediary exporter files
+		//		'Geometries.js',					// Ignore intermediary exporter files
+		//		'Materials.js',						// Ignore intermediary exporter files
 
 		'RaytracingWorker.js',
 		'ctm',                              // Todo: Need to check worker import
