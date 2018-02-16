a89be4a1b2296ade892ba695d8d5feb0bb106c6f
Tristan VALCKE <valcketristan@gmail.com>
2018-02-16 08:09:02 +0100

    Es6Convertor: Clean up

--- es6-convertor.js
+++ es6-convertor.js
@@ -415,33 +415,6 @@ function _createExportMap ( filesPaths, edgeCases, outputBasePath ) {
 
     } )
 
-    // LOG
-    //    console.log( 'exportMap:' + JSON.stringify( orderKeys( _exportMap ), null, 4 ) );
-    //    console.log( 'revertExportMap:' + JSON.stringify( orderKeys( _revertExportMap ), null, 4 ) );
-    //    function orderKeys ( obj ) {
-    //
-    //        var keys = Object.keys( obj ).sort( function keyOrder ( k1, k2 ) {
-    //            if ( k1 < k2 ) {
-    //                return -1
-    //            } else if ( k1 > k2 ) {
-    //                return +1
-    //            } else {
-    //                return 0
-    //            }
-    //        } )
-    //
-    //        var i, after = {}
-    //        for ( i = 0 ; i < keys.length ; i++ ) {
-    //            after[ keys[ i ] ] = obj[ keys[ i ] ]
-    //            delete obj[ keys[ i ] ]
-    //        }
-    //
-    //        for ( i = 0 ; i < keys.length ; i++ ) {
-    //            obj[ keys[ i ] ] = after[ keys[ i ] ]
-    //        }
-    //        return obj
-    //    }
-
 }
 
 function _createFilesMap ( filesPaths, edgeCases, outputBasePath ) {
