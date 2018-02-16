3592bf14dfdb9f41a232c135589cb1d6492ad6c4
Tristan VALCKE <valcketristan@gmail.com>
2018-02-16 14:06:41 +0100

    Es6Config: Fix NodePass imports

--- configs/es6.conf.js
+++ configs/es6.conf.js
@@ -372,6 +372,11 @@ module.exports = {
 		NodeMaterial: {
 			imports: [ 'NodeLib' ]
 		},
+		NodePass: {
+			imports: [
+				'_Math'
+			]
+		},
 		NURBSCurve: {
 			imports: [ 'NURBSUtils' ]
 		},
