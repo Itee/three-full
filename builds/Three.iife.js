3cc6740c736ac99468578560307c1a5115186845
Tristan VALCKE <valcketristan@gmail.com>
2018-02-19 10:16:36 +0100

    Release 5.0.1

--- builds/Three.iife.js
+++ builds/Three.iife.js
@@ -12018,87 +12018,12 @@ var Three = (function (exports) {
 
   } );
 
-  function LineSegments( geometry, material ) {
-
-  	Line.call( this, geometry, material );
-
-  	this.type = 'LineSegments';
-
-  }
-
-  LineSegments.prototype = Object.assign( Object.create( Line.prototype ), {
-
-  	constructor: LineSegments,
-
-  	isLineSegments: true,
-
-  	computeLineDistances: ( function () {
-
-  		var start = new Vector3();
-  		var end = new Vector3();
-
-  		return function computeLineDistances() {
-
-  			var geometry = this.geometry;
-
-  			if ( geometry.isBufferGeometry ) {
-
-  				// we assume non-indexed geometry
-
-  				if ( geometry.index === null ) {
-
-  					var positionAttribute = geometry.attributes.position;
-  					var lineDistances = [];
-
-  					for ( var i = 0, l = positionAttribute.count; i < l; i += 2 ) {
-
-  						start.fromBufferAttribute( positionAttribute, i );
-  						end.fromBufferAttribute( positionAttribute, i + 1 );
-
-  						lineDistances[ i ] = ( i === 0 ) ? 0 : lineDistances[ i - 1 ];
-  						lineDistances[ i + 1 ] = lineDistances[ i ] + start.distanceTo( end );
-
-  					}
-
-  					geometry.addAttribute( 'lineDistance', new Float32BufferAttribute( lineDistances, 1 ) );
-
-  				} else {
-
-  					console.warn( 'LineSegments.computeLineDistances(): Computation only possible with non-indexed BufferGeometry.' );
-
-  				}
-
-  			} else if ( geometry.isGeometry ) {
-
-  				var vertices = geometry.vertices;
-  				var lineDistances = geometry.lineDistances;
-
-  				for ( var i = 0, l = vertices.length; i < l; i += 2 ) {
-
-  					start.copy( vertices[ i ] );
-  					end.copy( vertices[ i + 1 ] );
-
-  					lineDistances[ i ] = ( i === 0 ) ? 0 : lineDistances[ i - 1 ];
-  					lineDistances[ i + 1 ] = lineDistances[ i ] + start.distanceTo( end );
-
-  				}
-
-  			}
-
-  			return this;
-
-  		};
-
-  	}() )
-
-  } );
-
   function Line( geometry, material, mode ) {
 
   	if ( mode === 1 ) {
 
-  		console.warn( 'Line: parameter LinePieces no longer supported. Created LineSegments instead.' );
-  		return new LineSegments( geometry, material );
+  		console.error( 'Line: parameter LinePieces no longer supported. Created LineSegments instead.' );
+  		
 
   	}
 
@@ -12149,7 +12074,7 @@ var Three = (function (exports) {
 
   				} else {
 
-  					console.warn( 'Line.computeLineDistances(): Computation only possible with non-indexed BufferGeometry.' );
+  					console.error( 'Line.computeLineDistances(): Computation only possible with non-indexed BufferGeometry.' );
 
   				}
 
@@ -31825,6 +31750,81 @@ var Three = (function (exports) {
   	return this;
 
   };
+
+  function LineSegments( geometry, material ) {
+
+  	Line.call( this, geometry, material );
+
+  	this.type = 'LineSegments';
+
+  }
+
+  LineSegments.prototype = Object.assign( Object.create( Line.prototype ), {
+
+  	constructor: LineSegments,
+
+  	isLineSegments: true,
+
+  	computeLineDistances: ( function () {
+
+  		var start = new Vector3();
+  		var end = new Vector3();
+
+  		return function computeLineDistances() {
+
+  			var geometry = this.geometry;
+
+  			if ( geometry.isBufferGeometry ) {
+
+  				// we assume non-indexed geometry
+
+  				if ( geometry.index === null ) {
+
+  					var positionAttribute = geometry.attributes.position;
+  					var lineDistances = [];
+
+  					for ( var i = 0, l = positionAttribute.count; i < l; i += 2 ) {
+
+  						start.fromBufferAttribute( positionAttribute, i );
+  						end.fromBufferAttribute( positionAttribute, i + 1 );
+
+  						lineDistances[ i ] = ( i === 0 ) ? 0 : lineDistances[ i - 1 ];
+  						lineDistances[ i + 1 ] = lineDistances[ i ] + start.distanceTo( end );
+
+  					}
+
+  					geometry.addAttribute( 'lineDistance', new Float32BufferAttribute( lineDistances, 1 ) );
+
+  				} else {
+
+  					console.warn( 'LineSegments.computeLineDistances(): Computation only possible with non-indexed BufferGeometry.' );
+
+  				}
+
+  			} else if ( geometry.isGeometry ) {
+
+  				var vertices = geometry.vertices;
+  				var lineDistances = geometry.lineDistances;
+
+  				for ( var i = 0, l = vertices.length; i < l; i += 2 ) {
+
+  					start.copy( vertices[ i ] );
+  					end.copy( vertices[ i + 1 ] );
+
+  					lineDistances[ i ] = ( i === 0 ) ? 0 : lineDistances[ i - 1 ];
+  					lineDistances[ i + 1 ] = lineDistances[ i ] + start.distanceTo( end );
+
+  				}
+
+  			}
+
+  			return this;
+
+  		};
+
+  	}() )
+
+  } );
 
   function LineLoop( geometry, material ) {
 
