0314b6d39ab5d4a3763dd31cb5e9c79f817cd642
Tristan VALCKE <valcketristan@gmail.com>
2018-09-03 21:51:37 +0200

    RollupTestsConfig: Filter files path

--- configs/rollup.tests.conf.js
+++ configs/rollup.tests.conf.js
@@ -74,6 +74,61 @@ function _getFilesPathsUnder ( filePaths ) {
 }
 
 /**
+ * Will create an array without the strings in filePaths that are matched in excludes paths
+ *
+ * @param {Array.<string>} filePaths - An array of string to clean
+ * @param {Array.<string>} excludes - The paths to remove
+ * @return {Array.<string>} The cleaned filePaths of excludes paths
+ * @private
+ */
+function _excludesFilesPaths ( filePaths, excludes ) {
+
+    let filteredFilesPath = []
+
+    let filePath = undefined
+    for ( let filePathIndex = 0, numberOfFilePaths = filePaths.length ; filePathIndex < numberOfFilePaths ; filePathIndex++ ) {
+        filePath = filePaths[ filePathIndex ]
+
+        if ( isExclude( filePath ) ) {
+            console.log( 'Exclude: ' + filePath )
+            continue
+        }
+
+        filteredFilesPath.push( filePath )
+
+    }
+
+    return filteredFilesPath
+
+    function isExclude ( path ) {
+
+        let isExclude      = false
+        let excludePattern = undefined
+        for ( let i = 0, pathLength = excludes.length ; i < pathLength ; i++ ) {
+
+            excludePattern = excludes[ i ]
+
+            // In case this is a file name it must fully match
+            if ( excludePattern.indexOf( '.' ) > -1 ) {
+
+                const fileName = path.replace( /^.*(\\|\/|\:)/, '' )
+                if ( fileName === excludePattern ) {
+                    isExclude = true
+                }
+
+            } else if ( path.contains( excludePattern ) ) {
+                isExclude = true
+            }
+
+        }
+
+        return isExclude
+
+    }
+
+}
+
+/**
  * Will filter file paths an keep only js files
  *
  * @param {Array.<string>} filePaths - An array of path to filter
@@ -133,11 +188,19 @@ function glsl () {
 
 function RollupTestConfigurationBuilder () {
 
-    const basePath         = path.join( __dirname, '..' )
-    const sourcesPath      = path.join( basePath, 'sources' )
-    const testsPath        = path.join( basePath, 'tests' )
-    const sourcesFilesPath = _getFilesPathsUnder( sourcesPath )
-    const jsFilesPath      = _filterJavascriptFiles( sourcesFilesPath )
+    const excludes = [
+        'Three.js',
+        'polyfills.js',
+        'utils.js',
+        'constants.js',
+    ]
+
+    const basePath            = path.join( __dirname, '..' )
+    const sourcesPath         = path.join( basePath, 'sources' )
+    const testsPath           = path.join( basePath, 'tests' )
+    const sourcesFilesPath    = _getFilesPathsUnder( sourcesPath )
+    const availableFilesPaths = _excludesFilesPaths( sourcesFilesPath, excludes )
+    const jsFilesPath         = _filterJavascriptFiles( availableFilesPaths )
 
     const configs = []
 
