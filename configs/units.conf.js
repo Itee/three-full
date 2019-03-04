4a59bed2c80a8f0d7378b4244ece5ca2fa1d4766
Tristan VALCKE <valcketristan@gmail.com>
2019-03-04 09:40:47 +0100

    Add units config file to allow edge case in unit test generation

---
+++ configs/units.conf.js
@@ -0,0 +1,11 @@
+/**
+ * @author [Tristan Valcke]{@link https://github.com/Itee}
+ * @license [BSD-3-Clause]{@link https://opensource.org/licenses/BSD-3-Clause}
+ *
+ * @file Todo
+ *
+ * @example Todo
+ *
+ */
+
+module.exports = {}
