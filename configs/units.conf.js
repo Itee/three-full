979985da22fd24c309723db8ae4078cbf83ddb86
Tristan VALCKE <valcketristan@gmail.com>
2020-02-12 01:03:00 +0100

    UnitsConfig: Use correct mapping about filename and exported stuff

--- configs/units.conf.js
+++ configs/units.conf.js
@@ -8,4 +8,47 @@
  *
  */
 
-module.exports = {}
+module.exports = {
+    '3MFLoader': {
+        exports: [ 'ThreeMFLoader' ]
+    },
+    CurveExtras: {
+        exports: [
+            'GrannyKnot',
+            'HeartCurve',
+            'VivianiCurve',
+            'KnotCurve',
+            'HelixCurve',
+            'TrefoilKnot',
+            'TorusKnot',
+            'CinquefoilKnot',
+            'TrefoilPolynomialKnot',
+            'FigureEightPolynomialKnot',
+            'DecoratedTorusKnot4a',
+            'DecoratedTorusKnot4b',
+            'DecoratedTorusKnot5a',
+            'DecoratedTorusKnot5c'
+        ]
+    },
+    Interpolations: {
+        exports: [ 'CatmullRom', 'QuadraticBezier', 'CubicBezier' ]
+    },
+    Math: {
+        exports: [ '_Math' ]
+    },
+    RollerCoaster: {
+        exports: [
+            'RollerCoasterGeometry',
+            'RollerCoasterLiftersGeometry',
+            'RollerCoasterShadowGeometry',
+            'SkyGeometry',
+            'TreesGeometry'
+        ]
+    },
+    ShaderTranslucent: {
+        exports: [ 'TranslucentShader' ]
+    },
+    WebVRUtils: {
+        exports: [ 'setProjectionFromUnion' ]
+    },
+}
