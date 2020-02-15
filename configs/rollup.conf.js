f8abaec7245ed009c6df225a8edf2a4c95870961
Tristan VALCKE <valcketristan@gmail.com>
2020-02-15 16:12:53 +0100

    RollupConfig: Add deprecation warning

--- configs/rollup.conf.js
+++ configs/rollup.conf.js
@@ -17,6 +17,12 @@ module.exports = function rollupConfigure ( format, onProduction, wantSourceMap 
     const outputFilePath = path.join( __dirname, '..', 'builds/' + fileName + '.' + _format + fileExtension )
 
     let banner = '// Made by Itee (https://github.com/Itee) with ES6 Convertor script\n\n'
+
+    // Add deprecation message
+    if( !_onProduction ) {
+        banner += `console.warn('[ThreeFull]: I am glad to annonce since Three release his examples files under JSM folder, this repository should be avoided. The first goal of this repository was to provided an alternative solution during this migration. From now, please consider to use Three.module.js instead even if i maintain this repository for my personal usage. Thanks for all ! Itee...')`
+    }
+
     if ( _format === 'cjs' ) {
         banner += '' +
             'var DEBUG = (process && process.env && process.env.Debug);\n' +
